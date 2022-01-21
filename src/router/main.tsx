b7c2f52acc40be9cf2a261ef91bdd24b5ee5cda4
LamWolf <lamwolfwang@gmail.com>
2022-01-21 16:28:09 +0800

    add lazyLoad & spin

--- src/router/main.tsx
+++ src/router/main.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Main from '@/pages/main';
-import Login from '@/pages/login';
+import { Spin } from 'antd';
+import './main.css';
 
-const MainRouter = () => (
+const Main = React.lazy(() => import('@/pages/main'));
+const Login = React.lazy(() => import('@/pages/login'));
+
+function suspensFunc(elementNode: ReactElement): ReactElement {
+  return (
+    <React.Suspense fallback={<Spin className='spin-loading' />}>{elementNode}</React.Suspense>
+  );
+}
+
+const MainRouter: React.FC<ReactElement> = () => (
   <Routes>
-    <Route path='/' element={<Main />} />
-    <Route path='/login' element={<Login />} />
+    <Route path='/' element={suspensFunc(<Main />)} />
+    <Route path='/login' element={suspensFunc(<Login />)} />
   </Routes>
 );
 
